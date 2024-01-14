1693e8978b98ef302c21ac5d3a21030d81875a7a
David Goldhirsch <dgoldhirsch@wayfair.com>
2024-01-14 17:47:29 -0500

    More churn

--- app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
+++ app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
