f01b7ca44b4a5d52ad469087b0a3d30438633761
David Goldhirsch <dgoldhirsch@wayfair.com>
2024-01-14 10:49:25 -0500

    LazyColumn

--- app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
+++ app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
@@ -1,7 +1,9 @@
 package com.example.takehome.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
@@ -10,8 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.takehome.Product
 import com.example.takehome.R
 import com.example.takehome.roundToNearestHalf
@@ -19,11 +24,24 @@ import com.example.takehome.roundToNearestHalf
 @Composable
 fun ProductsLayout(viewModel: ProductsViewModel) {
     val productsUiState by viewModel.uiState.collectAsState()
-
     when {
         productsUiState.isError -> Error(productsUiState.errorMessage)
         productsUiState.isLoading -> Loading()
+        productsUiState.products.isEmpty() -> Empty()
         else -> Products(productsUiState.products)
+    }
+}
+
+@Composable
+fun Empty() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = stringResource(id = R.string.no_products),
+        )
     }
 }
 
@@ -33,14 +51,11 @@ fun Error(message: String = stringResource(R.string.no_details)) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(stringResource(id = R.string.error, message))
+        Text(
+            textAlign = TextAlign.Center,
+            text = stringResource(id = R.string.error, message),
+        )
     }
-}
-
-@Composable
-@Preview
-fun ErrorPreview() {
-    Error("Kneecaps broke")
 }
 
 @Composable
@@ -49,30 +64,37 @@ fun Loading(text: String = stringResource(R.string.loading)) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(text)
+        Text(
+            textAlign = TextAlign.Center,
+            text = text,
+        )
     }
+}
+
+@Composable
+fun Products(products: List<Product>) {
+    LazyColumn {
+        items(products) {
+            Product(it)
+            Divider()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun EmptyPreview() = Empty()
+
+@Composable
+@Preview
+fun ErrorPreview() {
+    Error("Kneecaps broke")
 }
 
 @Composable
 @Preview
 fun LoadingPreview() {
     Loading()
-}
-
-@Composable
-fun Products(products: List<Product>) {
-    Column {
-        if (products.isEmpty()) {
-            Text(stringResource(R.string.no_products))
-        } else {
-            LazyColumn {
-                items(products) {
-                    Product(it)
-                    Divider()
-                }
-            }
-        }
-    }
 }
 
 @Composable
@@ -92,8 +114,13 @@ fun ProductsPreview() {
 
 @Composable
 private fun Product(product: Product) {
-    Text(product.name)
-    Text(product.tagline)
-    Text(product.rating.roundToNearestHalf().toString())
-    Text(product.date)
+    Column(
+        modifier = Modifier.padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        Text(product.name)
+        Text(product.tagline)
+        Text(product.rating.roundToNearestHalf().toString())
+        Text(product.date)
+    }
 }
