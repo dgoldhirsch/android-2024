9fc75843293b2d9107bcfdee8ab74e6001ca5997
David Goldhirsch <dgoldhirsch@wayfair.com>
2024-01-13 22:18:41 -0500

    Primitive Error and Loading UI

--- app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
+++ app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
@@ -1,5 +1,6 @@
 package com.example.takehome.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -8,7 +9,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.takehome.Product
 import com.example.takehome.R
 import com.example.takehome.roundToNearestHalf
@@ -16,14 +19,51 @@ import com.example.takehome.roundToNearestHalf
 @Composable
 fun ProductsLayout(viewModel: ProductsViewModel) {
     val productsUiState by viewModel.uiState.collectAsState()
-    Products(productsUiState.products)
+
+    when {
+        productsUiState.isError -> Error(productsUiState.errorMessage)
+        productsUiState.isLoading -> Loading()
+        else -> Products(productsUiState.products)
+    }
+}
+
+@Composable
+fun Error(message: String = stringResource(R.string.no_details)) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(stringResource(id = R.string.error, message))
+    }
+}
+
+@Composable
+@Preview
+fun ErrorPreview() {
+    Error("Kneecaps broke")
+}
+
+@Composable
+fun Loading(text: String = stringResource(R.string.loading)) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text)
+    }
+}
+
+@Composable
+@Preview
+fun LoadingPreview() {
+    Loading()
 }
 
 @Composable
 fun Products(products: List<Product>) {
     Column {
         if (products.isEmpty()) {
-            Text(stringResource(R.string.Loading___))
+            Text(stringResource(R.string.no_products))
         } else {
             LazyColumn {
                 items(products) {
@@ -36,8 +76,24 @@ fun Products(products: List<Product>) {
 }
 
 @Composable
+@Preview
+fun ProductsPreview() {
+    Products(
+        listOf(
+            Product(
+                name = "MegaFlame Blow Torch",
+                tagline = "Once you've used this on them, they won't have much more to say to you or anyone else.",
+                rating = 4.5,
+                date = "1-13-2018",
+            ),
+        ),
+    )
+}
+
+@Composable
 private fun Product(product: Product) {
     Text(product.name)
     Text(product.tagline)
     Text(product.rating.roundToNearestHalf().toString())
+    Text(product.date)
 }
--- app/src/main/java/com/example/takehome/ui/ProductsUiState.kt
+++ app/src/main/java/com/example/takehome/ui/ProductsUiState.kt
@@ -5,23 +5,27 @@ import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
 data class ProductsUiState(
+    val errorMessage: String = "",
     val isError: Boolean = false,
     val isLoading: Boolean = true,
     val products: ImmutableList<Product> = persistentListOf(),
     val retryNumber: Int = 0,
 ) {
     fun asError(retryNumber: Int, errorMessage: String): ProductsUiState = copy(
+        errorMessage = errorMessage,
         isError = true,
         isLoading = false,
         retryNumber = retryNumber,
     )
 
     fun asLoading(): ProductsUiState = copy(
+        errorMessage = "",
         isError = false,
         isLoading = true,
     )
 
     fun asSuccess(products: ImmutableList<Product>): ProductsUiState = copy(
+        errorMessage = "",
         isError = false,
         isLoading = false,
         products = products,
