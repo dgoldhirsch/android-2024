4e3433e8c870992372b5befba9c318ad187ee0e8
David Goldhirsch <dgoldhirsch@wayfair.com>
2024-01-14 10:50:02 -0500

    Churn

--- app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
+++ app/src/main/java/com/example/takehome/ui/ProductsLayout.kt
@@ -1,7 +1,6 @@
 package com.example.takehome.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
